d9a50e6b770357f0a40f43ebd002b7d806e341a1
Sankalp-sa <sankalp.p@ahduni.edu.in>
2023-02-22 13:43:00 +0530

    Completed TODO List project

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,21 +1,58 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const bp = require('body-parser');
+const date = require(__dirname+"/date.js");
 
-const app = express();
+let app = express();
 
-app.get("/",function(req,res){
+app.set('view engine', 'ejs');
+app.use(bp.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-    var today = new Date();
 
-    if(today.getDay() === 6 || today.getDay() === 0){
-        res.write("Yay, its a weekend");
+var todos = ["Eat","Code", "Sleep", "Repeat"];
+let workItems = [];
+
+app.get("/" , function(req, res){
+
+    let day = date.getDate();
+    res.render('list',{listTitle: day, newItem: todos});
+
+})
+
+app.post("/", function(req, res){
+
+    let userInp = req.body.UserInp;
+    
+    if(req.body.list == "Work List"){
+        workItems.push(userInp);
+        res.redirect("/work");
     }
     else{
-        res.sendFile(__dirname+"/index.html");
+        todos.push(userInp);
+        res.redirect('/');
     }
+    
+    
+})
+
+app.get('/work', function(req,res){
+
+    res.render('list',{listTitle: "Work List", newItem: workItems});
+})
+
+app.get('/about', function(req,res){
+    res.render("about");
+})
+
+app.post('/work', function(req,res){
+
+    var userInp = req.body.UserInp;
+    todos.push(userInp);
+    res.redirect('/work');
 
 })
 
-app.listen(3000, function(){
-    console.log("Servering running on port 3000")
-})
+
+app.listen(3000, ()=>{
+    console.log('Server running on port 3000');
+});

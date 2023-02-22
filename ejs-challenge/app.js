87d5559816b3b03d0877bfd6edeb29ac7e0da013
Sankalp-sa <sankalp.p@ahduni.edu.in>
2023-02-22 14:36:41 +0530

    Added header and footer files to partials folder

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -16,17 +16,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", function(req,res){
+  res.render('home',{homePara: homeStartingContent});
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
